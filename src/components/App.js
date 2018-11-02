c7d10efb3420352e61c6fcfe82d348c1c2939dc2
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-02 14:37:48 -0500

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="default-title">Some Futuristic App Here@@!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
