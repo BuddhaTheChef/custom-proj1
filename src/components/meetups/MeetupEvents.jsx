bea3e080405203a06efb5fe90b1f4cc50527a8c7
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-06 20:38:23 -0600

    imported meetup api

--- src/components/meetups/MeetupEvents.jsx
+++ src/components/meetups/MeetupEvents.jsx
@@ -2,17 +2,78 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class MeetupEvents extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      isLoaded: false,
+      query: ''
+
+    }
+  }
+
+  componentDidMount() {
+    this.search('');
+  }
+
+  changeCity() {
+    console.log(this.state)
+    this.setState({
+      query: this.state.query
+    })
+    this.search(this.state.query)
+  }
+
+  search = query => {
+
+
+    let api = 'https://api.meetup.com/find/groups?&sign=true&key=';
+    let apikey = `${process.env.REACT_APP_API_KEY}`;
+    let params = '&photo-host=public';
+    let location = `&location=${query}&page=20`;
+    let url = api + apikey + params + location;
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+      console.log(json)
+    })
+  }
+
   render() {
-    return (
-      <div>
-        <h1 className="">Meetup Events</h1>
-        <h3>
-        City: <input id="city" placeholder="Seattle"></input>
-        <br />
-        <button id="submit-city">Submit</button>
-        </h3>
-      </div>
-    );
+
+    let {isLoaded, items} = this.state;
+
+    if(!isLoaded) {
+      return <div>Loading...</div>
+    }
+    else {
+      return (
+        <div>
+          <h1 className="">Meetup Events</h1>
+          <h3>
+          City: <input id="city" refs="textbox" onChange={event=> this.setState({query: event.target.value})} placeholder="Seattle"/>
+          <br />
+          <button id="submit-city" onClick={() => this.changeCity()}>Submit</button>
+          </h3>
+          <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              Name: {item.name} | location: {item.city}, {item.state} | {item.category.name}
+              <br />
+                <img alt="group_pic" src={item.key_photo.photo_link} />
+              <hr />
+            </li>
+          ))}
+          </ul>
+        </div>
+      )
+    }
   }
 }
 
