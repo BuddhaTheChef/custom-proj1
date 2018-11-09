e2e06f7ee82932bd60b6572eaae39bc0979412b8
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-08 19:19:18 -0600

    added in styles for meetup

--- src/components/meetups/MeetupEvents.jsx
+++ src/components/meetups/MeetupEvents.jsx
@@ -45,6 +45,11 @@ class MeetupEvents extends Component {
     })
   }
 
+  // formatTime(jsonDate) {
+  // var newDate = dateFormat(jsonDate, "mm/dd/yyyy");
+  // return newDate;
+  // }
+
   render() {
 
     let {isLoaded, items} = this.state;
@@ -52,23 +57,37 @@ class MeetupEvents extends Component {
     if(!isLoaded) {
       return <div>Loading...</div>
     }
+
     else {
       return (
         <div>
           <h1 className="">Meetup Events</h1>
           <h3>
-          City: <input id="city" refs="textbox" onChange={event=> this.setState({query: event.target.value})} placeholder="Seattle"/>
+          City: <input id="city" refs="textbox" onChange={event=> this.setState({query: event.target.value})} placeholder="Search by city"/>
           <br />
           <button id="submit-city" onClick={() => this.changeCity()}>Submit</button>
           </h3>
-          <ul>
+          <ul className="ul-event">
           {items.map(item => (
-            <li key={item.id}>
-              Name: {item.name} | location: {item.city}, {item.state} | {item.category.name}
-              <br />
-                <img alt="group_pic" src={item.key_photo.photo_link} />
-              <hr />
+            <div className="main-event-div" key={item.id}>
+            <li className="li-event" >
+            <div className="event-img-div">
+            <img className="event-img" alt="group_pic" src={item.key_photo.photo_link} />
+            </div>
+            <div className="event-div-info">
+              <h1> {item.name} </h1>
+              <h3>Location: {item.city}, {item.state} | <span> Event Type: {item.category.name}</span> </h3>
+              <h3>When: {new Intl.DateTimeFormat('en-GB', {
+          year: 'numeric',
+          month: 'long',
+          day: '2-digit'
+        }).format(item.created)}</h3>
+              <div className="about-event">
+              About: {item.description}
+              </div>
+            </div>
             </li>
+            </div>
           ))}
           </ul>
         </div>
