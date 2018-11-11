1bf87ffbfe409b422355de4306a0e067992399d6
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-10 22:07:05 -0600

    added in sign in

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import '../App.css';
+import {Button} from 'semantic-ui-react'
 
 class HomePage extends Component {
   render() {
     return (
       <div className="main-landing-div">
       <div className="foreground">
-
       </div>
+      <div className="main-info-div">
       <h1 className="default-title">The Holy Grail for New Developers</h1>
+      <div className="main-info-button-div">
+      <Button color="purple">Log In</Button>
+      <Button color="purple">Sign Up</Button>
+      </div>
+      </div>
       </div>
     );
   }
