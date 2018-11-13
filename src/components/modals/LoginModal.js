a98bbdea3348b07ece866993654e3d7f5065496c
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-12 20:29:18 -0600

    added in styles for login modal

--- src/components/modals/LoginModal.js
+++ src/components/modals/LoginModal.js
@@ -1,6 +1,50 @@
 import React, { Component } from "react";
 import "../App.css";
 import PropTypes from "prop-types";
+import { Button } from 'semantic-ui-react'
+
+
+const loginModalHeader = {
+ textAlign: 'center',
+ marginTop: '0px',
+ fontSize: '36px'
+}
+
+const loginModalMainDiv = {
+  padding: '0px 20px 0px 20px',
+  marginBottom: '25px'
+}
+
+const loginModalInput = {
+  width: '100%',
+backgroundColor: 'white',
+borderRadius: '5px',
+border: '1px solid darkgray',
+height: '30px'
+
+}
+
+const loginSubmitButton = {
+  marginLeft: '41%',
+}
+
+const signUpWords = {
+  textAlign: 'center',
+marginTop: '25px',
+fontSize: '20px'
+}
+
+const loginCloseMainDiv = {
+  width:'100%',
+  textAlign: 'end'
+}
+
+const loginCloseModalButton = {
+borderRadius: '5px',
+border: 'magenta 1px solid',
+padding: '5px',
+color: 'magenta'
+}
 
 class LoginModal extends Component {
   render() {
@@ -8,23 +52,24 @@ class LoginModal extends Component {
     if (!this.props.show) {
       return null;
     }
-
     return (
       <div className="backdrop">
         <div className="modal">
-          <h1>Login</h1>
-          <h2>Yo</h2>
-          <input />
-          <h2>Yo</h2>
-          <input />
-          <h2>Yo</h2>
-          <input />
-          <button>Submit</button>
+        <div style={loginCloseMainDiv}>
+        <button style={loginCloseModalButton} onClick={this.props.onClose}>X</button>
+        </div>
+          <h1 style={loginModalHeader}>Login</h1>
+          <div style={loginModalMainDiv}>
+          <h2>E-mail:</h2>
+          <input style={loginModalInput} />
+          <h2>Password:</h2>
+          <input style={loginModalInput} />
+          </div>
+          <Button color="pink" style={loginSubmitButton}>Submit</Button>
           <br />
-          <p>Not a User? Singup.</p>
-          <button>Sign Up</button>
+          <p style={signUpWords}>Not a User? <span href="">Singup</span></p>
           <div className="footer">
-            <button onClick={this.props.onClose}>Close</button>
+
           </div>
         </div>
       </div>
