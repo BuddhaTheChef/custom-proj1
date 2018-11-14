77b96656e191ef18ba038df9a2e1d540a9124103
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-13 18:50:31 -0600

    added template for profile page

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { Button } from 'semantic-ui-react';
 
 class Profile extends Component {
   render() {
     return (
-      <h1>My Profile</h1>
+      <div className="main-div-profile">
+      <div className="profile-info-header">
+      <div className="profile-edit-header-button">
+      <Button inverted color="black">Edit Header</Button>
+      </div>
+      <div className="profile-image-div">Insert picture here</div>
+      <h1>First Name, Last Name</h1>
+      <h2>Aspirining [Enter Job Type Here]</h2>
+      <h3>- Enter Skill List Here -</h3>
+      </div>
+      <div className="profile-info-footer">
+      <div className="profile-footer-title-div">
+      <h1 className="profile-footer-title">Something Unique</h1>
+      </div>
+      <div className="profile-footer-main-content-div"> 
+      <h1>Some Content here</h1>
+      </div>
+      </div>
+      </div>
     );
   }
 }
